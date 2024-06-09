4a4938cda5efb384adfa5bf6a327c4aee8c40c17
Rafał <145366939+RafalC999@users.noreply.github.com>
2024-06-09 13:55:00 +0200

    Update Program.cs
    delete_comments

--- TodoList.Api/Program.cs
+++ TodoList.Api/Program.cs
@@ -3,12 +3,6 @@ namespace TodoList.Api
     public class Program
     {
 
-        //TODO: dodaæ generowanie tokenu po podaniu user password, rejestracja logowanie
-        // dodaæ do klasy BaseEntity ModifiedOn ModifiedBy, CreatedOn, CreatedBy migracja, update bazy
-        // dodaæ "AuditInterceptor" https://www.youtube.com/watch?v=mAlO3OuoQvo
-
-
-
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
